e9fde101bfddbe6c5585f0b671c6e3ae016a3f46
BrunoGC13 <brunoglotz@icloud.com>
2025-09-05 22:32:34 +0200

    Initial commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs').promises;
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3030;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
